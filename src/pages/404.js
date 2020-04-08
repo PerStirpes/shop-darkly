0c3d84a3fb11108c06992a4fe53c2b9e36ac89cb
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2020-04-07 20:02:56 -0700

    Update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import { Heading, Text, TextContainer } from '../components/shared/Typography';
+import React from 'react'
+import { Heading, Text, TextContainer } from '../components/shared/Typography'
 
 const NotFoundPage = () => (
   <TextContainer>
     <Heading>Whoops - That Page Doesn’t Exist (404)</Heading>
     <Text>
-      Looks like the page you requested either doesn’t exist or has been moved.
-      If you think this is an error or ended up at this page by following a
-      link, please{' '}
-      <a href="https://github.com/LaunchDarkly/issues/new">open an issue</a> to
-      let us know.
+      Questions? Concerns? Neuroses? If you think this is an error or ended up at this page by following a link, please{' '}
+      <a href="https://github.com/perstirpes/shop-darkly/issues/new">open an issue</a>. PRs Welcome 🙃
     </Text>
   </TextContainer>
-);
+)
 
-export default NotFoundPage;
+export default NotFoundPage
